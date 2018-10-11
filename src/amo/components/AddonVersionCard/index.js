0c43e4df1d39019cef37b8d6e1195e0282c9f4f5
Bob Silverberg <bob.silverberg@gmail.com>
2018-10-11 15:12:22 -0400

    More changes to AddonVersionCard, including copying some stuff over from Addon/index.js

--- src/amo/components/AddonVersionCard/index.js
+++ src/amo/components/AddonVersionCard/index.js
@@ -24,8 +24,50 @@ type InternalProps = {|
 |};
 
 export class AddonVersionCardBase extends React.Component<InternalProps> {
+  // From Addon/index.js
+  renderInstallError() {
+    const { i18n, installError: error } = this.props;
+
+    if (!error) {
+      return null;
+    }
+
+    return (
+      <Notice className="Addon-header-install-error" type="error">
+        {getErrorMessage({ i18n, error })}
+      </Notice>
+    );
+  }
+
   render() {
     const { i18n, userAgentInfo, version } = this.props;
+
+    // From  Addon/index.js - probably need some of this to determine compatibility of versions.
+    let isCompatible = false;
+    let compatibility;
+    if (addon) {
+      compatibility = getClientCompatibility({
+        addon,
+        clientApp,
+        userAgentInfo,
+      });
+      isCompatible = compatibility.compatible;
+    }
+
+    const numberOfAddonsByAuthors = addonsByAuthors
+      ? addonsByAuthors.length
+      : 0;
+
+    const isFireFox =
+      compatibility && compatibility.reason !== INCOMPATIBLE_NOT_FIREFOX;
+    const showInstallButton = addon && isFireFox;
+    const showGetFirefoxButton = addon && !isFireFox;
+
+    //
+    //
+
+
+
     const fileInfo = getVersionInfo({ userAgentInfo, version });
     const fileInfoText = fileInfo ? (
       <div className="AddonVersionCard-fileInfo">
@@ -39,16 +81,89 @@ export class AddonVersionCardBase extends React.Component<InternalProps> {
       </div>
     ) : null;
     const releaseNotes = sanitizeUserHTML(version.releaseNotes);
-    // TODO: Make it a link.
-    const licenceText = i18n.sprintf(
-      i18n.gettext('Source code released under %(licenseName)s'),
-      {
-        licenseName: version.license.name,
-      },
-    );
+    // TODO: Make license name a link.
+    const licenseText = version.license.name
+      ? i18n.sprintf(
+          i18n.gettext('Source code released under %(licenseName)s'),
+          {
+            licenseName: version.license.name,
+          },
+        )
+      : i18n.gettext('Source code released under Custom license');
 
     return (
-      <li className="AddonVersionCard">
+
+      // This is directly beneath an element: <Card className="Addon-header-info-card" photonStyle>
+      {
+        this.renderInstallError();
+  }
+
+    {
+      isFireFox && !isCompatible ? (
+        <AddonCompatibilityError
+          className="Addon-header-compatibility-error"
+          downloadUrl={compatibility.downloadUrl}
+          maxVersion={compatibility.maxVersion}
+          minVersion={compatibility.minVersion}
+          reason={compatibility.reason}
+        />
+      ) : null;
+    }
+
+    //
+    //
+
+    <div className="Addon-summary-and-install-button-wrapper">
+      {showSummary ? (
+        <p className="Addon-summary" {...summaryProps} />
+      ) : null}
+
+      {showInstallButton &&
+      config.get('enableFeatureAMInstallButton') && (
+        <AMInstallButton
+          addon={addon}
+          defaultInstallSource={defaultInstallSource}
+          disabled={!isCompatible}
+          enable={enable}
+          hasAddonManager={hasAddonManager}
+          install={install}
+          installTheme={installTheme}
+          setCurrentStatus={setCurrentStatus}
+          status={installStatus}
+          uninstall={uninstall}
+          isAddonEnabled={isAddonEnabled}
+        />
+      )}
+      {showInstallButton &&
+      config.get('enableFeatureAMInstallButton') === false && (
+        <InstallButton
+          {...this.props}
+          disabled={!isCompatible}
+          defaultInstallSource={defaultInstallSource}
+          status={installStatus}
+          useButton
+        />
+      )}
+      {showGetFirefoxButton && (
+        <Button
+          buttonType="confirm"
+          href={`https://www.mozilla.org/firefox/new/${makeQueryStringWithUTM(
+            {
+              utm_content: addon.guid,
+            },
+          )}`}
+          puffy
+          className="Button--get-firefox"
+        >
+          {i18n.gettext('Only with Firefox—Get Firefox Now')}
+        </Button>
+      )}
+    </div>
+
+    //
+    //
+
+    <li className="AddonVersionCard">
         <h2 className="AddonVersionCard-version">{version.version}</h2>
         {fileInfoText}
         <div className="AddonVersionCard-compatibility">
@@ -59,7 +174,7 @@ export class AddonVersionCardBase extends React.Component<InternalProps> {
           className="AddonVersionCard-releaseNotes"
           dangerouslySetInnerHTML={releaseNotes}
         />
-        <div className="AddonVersionCard-license">{licenceText}</div>
+        <div className="AddonVersionCard-license">{licenseText}</div>
       </li>
     );
   }
