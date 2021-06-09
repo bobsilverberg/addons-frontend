becce16693152da8f8ff8787329fdca9e68cad37
Bob Silverberg <bob.silverberg@gmail.com>
2021-06-09 15:24:14 -0400

    Allow for excluding users from experiments based on values from state

--- src/amo/experiments/20210531_download_funnel_experiment.js
+++ src/amo/experiments/20210531_download_funnel_experiment.js
@@ -1,13 +1,21 @@
 /* @flow */
+import { isFirefox } from 'amo/utils/compatibility';
+import type { ExperimentConfig } from 'amo/withExperiment';
+
 export const VARIANT_CURRENT = 'current-link';
 export const VARIANT_NEW = 'new-link';
 
-export const EXPERIMENT_CONFIG = {
-  experimentConfig: {
-    id: '20210531_download_funnel_experiment',
-    variants: [
-      { id: VARIANT_CURRENT, percentage: 0.5 },
-      { id: VARIANT_NEW, percentage: 0.5 },
-    ],
+const experimentConfig: ExperimentConfig = {
+  id: '20210531_download_funnel_experiment',
+  variants: [
+    { id: VARIANT_CURRENT, percentage: 0.5 },
+    { id: VARIANT_NEW, percentage: 0.5 },
+  ],
+  // Exclude Firefox users.
+  shouldExcludeUser(state) {
+    const { userAgentInfo } = state.api;
+    return isFirefox({ userAgentInfo });
   },
 };
+
+export const EXPERIMENT_CONFIG = { experimentConfig };
